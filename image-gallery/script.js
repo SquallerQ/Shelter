883572e07370ef50532bf7356e1cde896548b40e
squallerq <john199502031@gmail.com>
2024-09-27 20:52:56 +0200

    feat: add new client-Id

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,17 +1,22 @@
-const url = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
+// const url = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo&per_page=30'
 
-async function getData() {
-  const res = await fetch(url)
+const startUrl = 'https://api.unsplash.com/search/photos?query='
+const clientID = "&client_id=i6nsRp4ITZAjlv7J1AxJF0MyC5VwiLyE2KCgBrFae3I";
+const startSearch = 'spring'
+const input = document.querySelector('.input')
+console.log(input.value);
+
+async function getData(_startUrl, _clientID, search) {
+  const res = await fetch(`${_startUrl}${search}${_clientID}&per_page=30`);
   const data = await res.json()
-  console.log(data);
-  console.log(data.results[0].urls.regular);
-  console.log(data);
+  // console.log(data.results[0].urls.regular);
     for (let i = 0; i < 30; i++) {
       showData(data.results[i].urls.regular);
     }
-
 }
-getData()
+getData(startUrl, clientID, startSearch);
+
+
 
 function showData(data) {
   const gallery = document.querySelector('.gallery')
